0940d570f9b8f7167ce813d56a8a3d75fd24d436
Sean Daniels <sdaniel5@ncsu.edu>
2021-02-28 13:11:59 -0500

    Upd

--- substring/main.cc
+++ substring/main.cc
@@ -1,34 +1,40 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
 class Solution{
     public:
       int substringLength(string s) {
-        vector<string> subStrings;
-        int max = 0, maxIndex = 0, counter = 0;
-        string existingChar = "", tempChar = "", tempString = "";
-        for (int i = 0; i < s.length(); i++) {
-          tempChar = s[i];
-          for (int ii = 0; ii < tempString.length(); ii++) {
-            existingChar = tempString[ii];
-            if (tempChar == existingChar) {
-                if(tempString.length()>max) max=tempString.length();
-                subStrings.push_back(tempString);
-                tempString = "";
-                continue;
-            }
+        if(s.length()==0) return 0;
+        int len = 0, max = 0, i = 0, j = 0;
+        vector<char> characters;
+        for(i = 0; i < s.length(); i++){
+          char c = s.at(i);
+          vector<char>::iterator it = find(characters.begin(), characters.end(), c);
+          while(it != characters.end()){
+            auto findChar = find(characters.begin(), characters.end(), s.at(j));
+            characters.erase(findChar);
+            ++j;
+            it = find(characters.begin(), characters.end(), c);
           }
-          tempString.append(tempChar);
+          characters.push_back(c);
+          #ifdef DBG
+          cout << "Char vector: ";
+          for(auto x : characters) cout << x << ",";
+          cout << endl;
+          #endif
+          len = i - j + 1;
+          if(len>max) max = len;
+          // i++;
         }
-        subStrings.push_back(tempString);
         return max;
       }
 };
 
 int main(int argc, char *argv[]) {
-    string check = "sass";
+    string check = "abadvdf";
     Solution mySolution;
     cout << mySolution.substringLength(check) << endl;
     return 0;
