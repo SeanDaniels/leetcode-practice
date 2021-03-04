5af2724c3345415b57c6c0f0562546f73d2f4c53
Sean Daniels <sdaniel5@ncsu.edu>
2021-03-04 17:18:04 -0500

    upd

--- dp/knapsack/knapsack.cpp
+++ dp/knapsack/knapsack.cpp
@@ -6,13 +6,15 @@ int main(){
     int maxWeight = 10;
     int weights[ITEMS] = {3, 2, 8, 7, 4};
     int values[ITEMS] = {2, 4, 6, 1, 9};
-    int maxWin = 0;
+    int minWin = 0, maxWin = 0;
     // what are all the possible combinations that are less than max weight?
-    for(int i = 0; i < ITEMS; i++){
-        int minWin = i;
-        maxWin = ITEMS;
-        for(int j = i; j < maxWin; j++){
-
+    for(int windowSize = 1; windowSize < 5; windowSize++){
+        maxWin = minWin + windowSize;
+        for(int i = 0; i < 5; i = i + windowSize){
+            for(int j = i + windowSize; j < 5; j = j + windowSize){
+                cout << "i: " << i << " j: "<< j << endl;
+                cout << weights[i] << ", " << weights[j] << endl;
+            }
         }
     }
 
