c99d53c8a2ed7ac8ace7fdbe1c7892528b0efc4e
Sean Daniels <sdaniel5@ncsu.edu>
2021-03-07 19:20:56 -0500

    upd

--- three-sum/threeSum.cpp
+++ three-sum/threeSum.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 class Solution{
     public:
-        void getThreeSets(vector<int> &mainV, vector<int> &subV, vector<vector<int>> &resultV, int idx){
-            for(int i = idx; i < int(mainV.size()); i++){
-                subV.push_back(mainV[i]);
-                if(subV.size()==3){
-                    resultV.push_back(subV);
-                }
-                getThreeSets(mainV, subV, resultV, i+1);
-                subV.pop_back();
-            }
-        }
         vector<vector<int>> threeSum(vector<int> &nums){
             vector<vector<int>> resultVector;
-            vector<int> subsets;
-            getThreeSets(nums, subsets, resultVector, 0);
-            for(auto x : resultVector){
-                for(auto y : x){
-                    cout << y << ",";
+            unordered_map<int, int> targetMap;
+            unordered_map<int, int>::iterator targetIt;
+            for(int i = 0; i < nums.size(); i++){
+                int tempTarget = 0 - nums[i];
+                targetIt = targetMap.find(tempTarget);
+                if(targetIt==targetMap.end()){
+                    targetMap.insert({tempTarget, tempTarget});
                 }
-                cout << endl;
+
             }
             return resultVector;
         }
